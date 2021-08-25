e20e4c335fbe543c8302bfae8c43e4b5b10c09ca
shane189 <shanechoi189@gmail.com>
2021-08-25 00:55:34 -0400

    api update for experiment

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,11 +1,14 @@
-//connect to the mongo server
 const mongoose = require('mongoose');
-//var uniqueValidator = require('mongoose-unique-validator');
 const dbName ="sciLab";
 
 mongoose.connect('mongodb://localhost/' + dbName,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 })
-.then(() => console.log("Connected with mongo database:" +dbName))
-.catch((err) => console.log("Something went wrong connecting to the DB:" +err))
+    .then(() => {
+        console.log("Connected with mongo database:" + dbName)
+    })
+    .catch((err) => {
+        console.log("Something went wrong connecting to the DB:");
+        console.log(err);
+    })
--- server/models/experiment.model.js
+++ server/models/experiment.model.js
@@ -4,36 +4,109 @@ const mongoose = require('mongoose');
 const ExperimentSchema = new mongoose.Schema(
     
     {
-        experimentName:{
+        experimentName: {
             type: String,
-            required:[true, "You must enter a name for experiment"],
-             },
-      
-
-        experimentDescription:{
-                type: String,
-             },
-
-        stepName:{ 
-            type: String
+            required:[true, "An experiment name is required"],
         },
-        stepDescription: { 
-            type: String
-         },
-        // tableName: { 
-        //     type: String
-        //  },
-        // table: {
-        //     type: table, 
-        //     default: 0
-        // }
 
-       },
-     {
-        timestamps:true,
-    }
-);
+        startDate: {
+            type: Date,
+            required:[true, "A start date is required"],
+        },
+
+        experimentNumber: { 
+            type: String,
+            required:[true, "An experiment number is required"],
+        },
+
+        objective: { 
+            type: String,
+            required:[true, "An objective is required"],
+        },
+
+        responsibleUser: { 
+            type: String,
+            required:[true, "An introduction is required"],
+        },
+
+        instrumentsRequired: { 
+            type: String,
+            required:[true, "A required instrument is required"],
+            enum: [
+                "Ammeter",
+                "Digital Scale",
+                "Multimeter",
+                "Oscilloscope",
+                "Vernier Caliper",
+                "Voltmeter",
+            ]
+        },
+
+        status: { 
+            type: String,
+            required:[true, "A status is required"],
+            enum: [
+                "New Request",
+                "In Queue",
+                "In Process",
+                "Completed",
+            ]
+        },
+
+        procedure: { 
+            type: String,
+            required:[true, "A status is required"],
+            enum: [
+                "Proc A",
+                "Proc B",
+                "Proc C",
+            ]
+        },
+
+        results: {
+            type: String,
+            required:[true, "A result is required"],
+        },
+
+        firstName: {
+            type: String,
+            required:[true, "A first name is required"],
+        },
+
+        lastName: {
+            type: String,
+            required:[true, "A last name is required"],
+        },
+
+        selectExperiment: {
+            type: String,
+            required:[true, "A select experiment is required"],
+            enum: [
+                "Experiment #1",
+                "Experiment #2",
+                "Experiment #3",
+                "Experiment #4",
+                "Experiment #5",
+                "Experiment #6",
+                "Experiment #7",
+                "Experiment #8",
+                "Experiment #9",
+            ]
+        },
+
+        type: {
+            type: String,
+            required:[true, "A type is required"],
+            enum: [
+                "PDF",
+                "DOC",
+                "HTML",
+            ]
+        },
+
+    }, { timestamps: true });
+
 const modelName = "Experiment";
 //ExperimentSchema.plugin(uniqueValidator);
-const  Experiment = mongoose.model(modelName,ExperimentSchema);
+const  Experiment = mongoose.model(modelName, ExperimentSchema);
 module.exports= Experiment;
