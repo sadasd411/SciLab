ace815e3f5975a378e460bfd452c55dd0b92bbea
devi.sada <devi.sada@gmail.com>
2021-08-26 10:41:54 -0400

    Procedure page integration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 // load .env file data --------------------------------------
 require("dotenv").config();
 
@@ -22,4 +21,16 @@ app.use(cookieParser());
 
 // configure mongoose to connect -----------------------------
 require('./config/mongoose.config');
+
+// add routes to listen --------------------------------------
+const experimentRoutes = require('./routes/experiment.route');    /// check if this is the right name
+experimentRoutes(app);
+
+require('./routes/user.routes')(app);           // this shud be fine coz using for login.
+// require('./routes/dashboard.routes')(app);        // dashboard here, check the name for this one.
+
+// set up the server for listening
+app.listen(port, () => {
+    console.log("The express app server is listening on port: ", port);
+})
 
